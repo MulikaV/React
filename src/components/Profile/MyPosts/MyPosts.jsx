5d720a6fdc4301be3b6187d4cef1ff7218dca146
mulikav <mulikavj@gmail.com>
2019-10-05 14:10:13 +0300

    lesson18

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Post from "./Post/Post";
+
+const MyPosts = () =>{
+  return(
+          <div>
+              My posts
+              <div>
+                  <textarea  ></textarea>
+                  <button>Add post</button>
+                  <button>Delete post</button>
+              </div>
+             <div className={s.posts}>
+                 <Post text = "My first post" />
+                 <Post text = "Wow It's working"/>
+
+             </div>
+          </div>
+  )
+};
+
+export default MyPosts;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import s from './Profile.module.css';
+import MyPosts from "./MyPosts/MyPosts";
+
+const Profile = () =>{
+  return(
+      <div className={s.content}>
+          <div>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5w2ItqPyvah_o6wduaZeMqjxXtG7HvQm8UlFwMHZk__VYciYcAg" alt=""/>
+          </div>
+          <div>
+              ava + description
+          </div>
+            <MyPosts />
+      </div>
+  )
+};
+
+export default Profile;
