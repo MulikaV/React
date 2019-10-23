d9974fff6c559959dcfb1cbcf8bddcd9d9c2af31
mulikav@gmail.com <mulikavj@gmail.com>
2019-10-23 12:04:23 +0300

    lesson 52

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
 import s from './Users.module.css';
+import * as axios from 'axios';
+import userAvatar from './../../assets/images/userimage.png'
 
 const Users = (props) => {
-  if (props.users.length === 0){
-    props.setUsers([
-      {
-        id: 1, avatarUrl: 'https://thumbs.dfs.ivi.ru/storage15/contents/a/d/13e9fd5d5a713d6c13c4b7ab19fa0d.jpeg',
-        followed: false, fullName: 'Dmitriy', status: 'I am a boss', location: {city: 'Minsk', country: 'Belarus'}
-      },
-      {
-        id: 2, avatarUrl: 'https://thumbs.dfs.ivi.ru/storage15/contents/a/d/13e9fd5d5a713d6c13c4b7ab19fa0d.jpeg',
-        followed: true, fullName: 'Valera', status: 'I am a boss', location: {city: 'Kiev', country: 'Ukraine'}
-      },
-      {
-        id: 3, avatarUrl: 'https://thumbs.dfs.ivi.ru/storage15/contents/a/d/13e9fd5d5a713d6c13c4b7ab19fa0d.jpeg',
-        followed: true, fullName: 'Oleg', status: 'I am a boss', location: {city: 'Moscow', country: 'Russia'}
-      },
-      {
-        id: 4, avatarUrl: 'https://thumbs.dfs.ivi.ru/storage15/contents/a/d/13e9fd5d5a713d6c13c4b7ab19fa0d.jpeg',
-        followed: false, fullName: 'Makar', status: 'I am a boss', location: {city: 'Zp', country: 'Ukraine'}
-      }
-    ]);
-  }
+  if (props.users.length === 0) {
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        props.setUsers(response.data.items);
+      });
 
+  }
 
   return (
     <div>
@@ -31,7 +19,7 @@ const Users = (props) => {
           <div key={u.id}>
             <span>
               <div>
-                <img src={u.avatarUrl} alt="avatar"/>
+                <img src={u.photos.small != null ? u.photos.small : userAvatar} alt="avatar"/>
               </div>
             </span>
             <span>
@@ -42,13 +30,13 @@ const Users = (props) => {
               </div>
             </span>
             <span>
-              <div>{u.fullName}</div>
+              <div>{u.name}</div>
               <div>{u.status}</div>
             </span>
-            <span>
+            {/*<span>
               <div>{u.location.city}</div>
               <div>{u.location.country}</div>
-            </span>
+            </span>*/}
           </div>
         ))
 
