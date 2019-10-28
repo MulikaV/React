87a3052fe2e282a7c33e07361009b52e8a3ae086
MulikaV <Mulikavj@gmail.com>
2019-10-28 21:52:16 +0200

    lesson56

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,73 +1,50 @@
 import React from 'react';
 import s from './Users.module.css';
-import * as axios from 'axios';
 import userAvatar from './../../assets/images/userimage.png'
-import {
-  Card, CardText, CardBody,
-  CardTitle, Button
-} from 'reactstrap';
+import {Card, CardText, CardBody,
+  CardTitle, Button} from 'reactstrap';
 import ReactPaginate from 'react-paginate';
 
-class Users extends React.Component {
-
-  componentDidMount() {
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-      .then(response => {
-        this.props.setUsers(response.data.items);
-        this.props.setTotalUsersCount(response.data.totalCount);
-      });
-  }
-
-  onPageChanged = (page) => {
-    this.props.setCurrentPage(page.selected + 1);
-    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page.selected + 1}&count=${this.props.pageSize}`)
-      .then(response => {
-        this.props.setUsers(response.data.items);
-      });
-  };
-
-  render() {
-
-    const totalPages = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-    return <div>
-
-      <div id="react-paginate">
-        <ReactPaginate
-          previousLabel={'Prev'}
-          nextLabel={'Next'}
-          breakLabel={'...'}
-          breakClassName={'break-me'}
-          pageCount={totalPages}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={3}
-          onPageChange={(e) => this.onPageChanged(e)}
-          containerClassName={'pagination'}
-          subContainerClassName={'pages pagination'}
-          activeClassName={'active'}
-        />
-      </div>
-      <div className={s.users}>
-        {
-          this.props.users.map(u => (
-            <Card key={u.id} className={s.userCard}>
-              <img className={s.userAvatar} src={u.photos.small != null ? u.photos.small : userAvatar} alt="avatar"/>
-              <CardBody>
-                <CardTitle>
-                  <div>{u.name}</div>
-                </CardTitle>
-                <CardText>
-                  <div>{u.status}</div>
-                </CardText>
-                {u.followed
-                  ? <Button onClick={() => this.props.unfollow(u.id)}>Unfollow</Button>
-                  : <Button onClick={() => this.props.follow(u.id)}>Follow</Button>}
-              </CardBody>
-            </Card>
-          ))
-        }
-      </div>
+const Users = (props) => {
+  const totalPages = Math.ceil(props.totalUsersCount / props.pageSize);
+  return <div>
+    <div id="react-paginate" className={s.center}>
+      <ReactPaginate
+        previousLabel={'Prev'}
+        nextLabel={'Next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={totalPages}
+        marginPagesDisplayed={1}
+        pageRangeDisplayed={3}
+        onPageChange={(e) => props.onPageChanged(e)}
+        containerClassName={'pagination'}
+        subContainerClassName={'pages pagination'}
+        activeClassName={'active'}
+      />
     </div>
-  }
-}
+
+    <div className={s.users}>
+      {
+        props.users.map(u => (
+          <Card key={u.id} className={s.userCard}>
+            <img className={s.userAvatar} src={u.photos.small != null ? u.photos.small : userAvatar} alt="avatar"/>
+            <CardBody>
+              <CardTitle>
+                <div>{u.name}</div>
+              </CardTitle>
+              <CardText>
+                <div>{u.status}</div>
+              </CardText>
+              {u.followed
+                ? <Button onClick={() => props.unfollow(u.id)}>Unfollow</Button>
+                : <Button onClick={() => props.follow(u.id)}>Follow</Button>}
+            </CardBody>
+          </Card>
+        ))
+      }
+    </div>
+  </div>
+};
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import Users from "./Users";
 import {connect} from "react-redux";
 import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../Redux/users-reduser";
+import * as axios from "axios/index";
+import Users from "./Users";
 
 const mapStateToProps = (state) => {
   return {
@@ -32,6 +33,32 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
+class UsersContainer extends React.Component {
 
-export default UsersContainer;
+  componentDidMount() {
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items);
+        this.props.setTotalUsersCount(response.data.totalCount);
+      });
+  }
+
+  onPageChanged = (page) => {
+    this.props.setCurrentPage(page.selected + 1);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page.selected + 1}&count=${this.props.pageSize}`)
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return  <Users totalUsersCount={this.props.totalUsersCount}
+                   pageSize={this.props.pageSize}
+                   onPageChanged={this.onPageChanged}
+                   users={this.props.users}
+                   unfollow={this.props.unfollow}
+                   follow={this.props.follow} />
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);;
